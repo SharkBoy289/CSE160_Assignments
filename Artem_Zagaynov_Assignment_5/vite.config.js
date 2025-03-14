6c7c6fe490064f5f78f1e82b67e2c0045446bb88
Artem <artemazag@gmail.com>
2025-03-13 22:47:36 -0700

    docs created

--- Artem_Zagaynov_Assignment_5/vite.config.js
+++ Artem_Zagaynov_Assignment_5/vite.config.js
@@ -3,15 +3,8 @@
  */
 export default {
   base: '/CSE160_Assignments/Artem_Zagaynov_Assignment_5/',
-  build: {
-    sourcemap: true,
-  },
-  // (Optional) Force bundling of three to avoid bare module errors:
+  build: { outDir: 'docs', sourcemap: true },
   optimizeDeps: {
-    include: [
-      'three',
-      'three/examples/jsm/libs/stats.module.js',
-      'three/addons/controls/OrbitControls.js'
-    ]
+    include: ['three', 'three/examples/jsm/libs/stats.module.js', 'three/addons/controls/OrbitControls.js']
   }
 }
