d4c87ce56a3065cb8d83fc4fcea697f942be9bc0
Artem <artemazag@gmail.com>
2025-03-13 22:03:42 -0700

    Update vite.config.js

--- Artem_Zagaynov_Assignment_5/vite.config.js
+++ Artem_Zagaynov_Assignment_5/vite.config.js
@@ -1,11 +1,18 @@
 /**
-* @type {import('vite').UserConfig}
-*/
+ * @type {import('vite').UserConfig}
+ */
 export default {
   base: '/CSE160_Assignments/Artem_Zagaynov_Assignment_5/',
   build: {
     outDir: "dist",
     sourcemap: true,
+  },
+  // (Optional) Force bundling of three to avoid bare module errors:
+  optimizeDeps: {
+    include: [
+      'three',
+      'three/examples/jsm/libs/stats.module.js',
+      'three/addons/controls/OrbitControls.js'
+    ]
   }
 }
-
