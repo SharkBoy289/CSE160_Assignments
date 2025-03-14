011613f6b030fbf90340a511eec26a86532c519b
Artem <artemazag@gmail.com>
2025-03-13 22:53:21 -0700

    Build for GitHub Pages

--- Artem_Zagaynov_Assignment_5/vite.config.js
+++ Artem_Zagaynov_Assignment_5/vite.config.js
@@ -2,9 +2,27 @@
  * @type {import('vite').UserConfig}
  */
 export default {
+  // If your GitHub Pages URL is:
+  // https://USERNAME.github.io/REPO_NAME/
+  // then base should be "/REPO_NAME/".
+  // For example, if your URL is:
+  // https://sharkboy289.github.io/CSE160_Assignments/Artem_Zagaynov_Assignment_5/
+  // your base is "/CSE160_Assignments/Artem_Zagaynov_Assignment_5/"
+
   base: '/CSE160_Assignments/Artem_Zagaynov_Assignment_5/',
-  build: { outDir: 'docs', sourcemap: true },
+
+  build: {
+    outDir: 'docs',   // Output built files to "docs" folder
+    sourcemap: true,
+  },
+
+  // (Optional) Force bundling of certain modules like three.js
+  // to avoid "bare module specifier" errors:
   optimizeDeps: {
-    include: ['three', 'three/examples/jsm/libs/stats.module.js', 'three/addons/controls/OrbitControls.js']
+    include: [
+      'three',
+      'three/examples/jsm/libs/stats.module.js',
+      'three/addons/controls/OrbitControls.js'
+    ]
   }
 }
